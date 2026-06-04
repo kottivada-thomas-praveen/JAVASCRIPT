aa3958e1a20022306bf41037ced111f9100eb187
kottivada thomas praveen <kottivadathomaspraveen@gmail.com>
2026-06-04 07:53:41 +0530

    counter program added

---
+++ counter/script.js
@@ -0,0 +1,21 @@
+const decreasebtn = document.getElementById("decreasebtn");
+const increasebtn = document.getElementById("increasebtn");
+const resetbtn = document.getElementById("resetbtn");
+let counter = document.getElementById("counter");
+
+let count = 0;
+
+decreasebtn.onclick = function(){
+    count--;
+    counter.textContent = count;
+}
+
+resetbtn.onclick = function(){
+    count = 0;
+    counter.textContent = count;
+}
+
+increasebtn.onclick = function(){
+    count++;
+    counter.textContent = count;
+}
